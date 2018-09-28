d9cae8a05bb6cddb525e4dd753abe503490b99c5
Robert Sumner Jr <rsumner868@icloud.com>
2018-09-28 05:22:40 -0500

    Merge html

--- .github/ISSUE_TEMPLATE.md
+++ .github/ISSUE_TEMPLATE.md
@@ -1,8 +1,21 @@
-<!--
-FOR SUPPORT REQUESTS PLEASE USE: support@greenkeeper.io
-Issues are primarily for tracking feature requests and non-security related bugs.
+If your issue is a **request for support in using Chainer**,
+please post it on [Stack Overflow](https://stackoverflow.com/questions/tagged/chainer).
 
-Please note that the Greenkeeper team is located in Berlin, and our response window is limited to EU workdays, meaning Monday to Friday, 9—17 CE(S)T.
+If it is a **bug report**, **feature request** or **suggestion**,
+please take a look at [our contribution guide](https://docs.chainer.org/en/stable/contribution.html).
 
-If you’ve discovered a SECURITY-RELATED BUG in Greenkeeper or related services, please disclose it to us confidentially by emailing us at support@greenkeeper.io
--->
+Specifically, if it is a bug report, these information are very helpful:
+
+* Conditions
+<!-- If you're using Chainer 4.0+, you can also get this information by typing `python -c 'import chainer; chainer.print_runtime_info()'. -->
+  - Chainer version
+  - CuPy version
+  - OS/Platform
+  - CUDA/cuDNN version
+  - etc.
+* Code to reproduce
+* Error messages, stack traces, or logs
+
+Thank you for your cooperation!
+
+Support is available on our [Slack Chat](https://bit.ly/join-chainer-slack).
--- setup.py
+++ setup.py
@@ -1,178 +1,206 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# pylint: disable=W0404,W0622,W0704,W0613
-# copyright 2003-2013 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
-# contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
-#
-# This file is part of pylint.
-#
-# pylint is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 2.1 of the License, or (at your option) any
-# later version.
-#
-# pylint is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License along
-# with pylint.  If not, see <http://www.gnu.org/licenses/>.
-"""Generic Setup script, takes package info from __pkginfo__.py file.
-"""
-from __future__ import absolute_import, print_function
-__docformat__ = "restructuredtext en"
 
 import os
+import pkg_resources
 import sys
-import shutil
-from os.path import isdir, exists, join
 
-try:
-    if os.environ.get('NO_SETUPTOOLS'):
-        raise ImportError()
-    from setuptools import setup
-    from setuptools.command import easy_install as easy_install_lib
-    from setuptools.command import install_lib
-    USE_SETUPTOOLS = 1
-except ImportError:
-    from distutils.core import setup
-    from distutils.command import install_lib
-    USE_SETUPTOOLS = 0
-    easy_install_lib = None
-
-from distutils.command.build_py import build_py
+from setuptools import setup
 
 
-base_dir = os.path.dirname(__file__)
+if sys.version_info[:3] == (3, 5, 0):
+    if not int(os.getenv('CHAINER_PYTHON_350_FORCE', '0')):
+        msg = """
+Chainer does not work with Python 3.5.0.
 
-__pkginfo__ = {}
-with open(os.path.join(base_dir, "pylint", "__pkginfo__.py")) as f:
-    exec(f.read(), __pkginfo__)
-modname = __pkginfo__['modname']
-distname = __pkginfo__.get('distname', modname)
-scripts = __pkginfo__.get('scripts', [])
-data_files = __pkginfo__.get('data_files', None)
-include_dirs = __pkginfo__.get('include_dirs', [])
-ext_modules = __pkginfo__.get('ext_modules', None)
-install_requires = __pkginfo__.get('install_requires', None)
-dependency_links = __pkginfo__.get('dependency_links', [])
+We strongly recommend to use another version of Python.
+If you want to use Chainer with Python 3.5.0 at your own risk,
+set CHAINER_PYTHON_350_FORCE environment variable to 1."""
+        print(msg)
+        sys.exit(1)
 
-readme_path = join(base_dir, 'README')
-if exists(readme_path):
-    with open(readme_path) as stream:
-        long_description = stream.read()
+
+def cupy_requirement(pkg):
+    return '{}==5.0.0rc1'.format(pkg)
+
+
+requirements = {
+    'install': [
+        'filelock',
+        'numpy>=1.9.0',
+        'protobuf>=3.0.0',
+        'six>=1.9.0',
+    ],
+    'cuda': [
+        cupy_requirement('cupy'),
+    ],
+    'stylecheck': [
+        'autopep8==1.3.5',
+        'flake8==3.5.0',
+        'pbr==4.0.4',
+        'pycodestyle==2.3.1',
+    ],
+    'test': [
+        'pytest',
+        'mock',
+    ],
+    'doctest': [
+        'sphinx==1.7.9',
+        'matplotlib',
+        'theano',
+    ],
+    'docs': [
+        'sphinx==1.7.9',
+        'sphinx_rtd_theme',
+    ],
+    'travis': [
+        '-r stylecheck',
+        '-r test',
+        '-r docs',
+        # pytest-timeout>=1.3.0 requires pytest>=3.6.
+        # TODO(niboshi): Consider upgrading pytest to >=3.6
+        'pytest-timeout<1.3.0',
+        'pytest-cov',
+        'theano',
+        'h5py',
+        'pillow',
+    ],
+    'appveyor': [
+        '-r test',
+        # pytest-timeout>=1.3.0 requires pytest>=3.6.
+        # TODO(niboshi): Consider upgrading pytest to >=3.6
+        'pytest-timeout<1.3.0',
+        'pytest-cov',
+    ],
+}
+
+
+def reduce_requirements(key):
+    # Resolve recursive requirements notation (-r)
+    reqs = requirements[key]
+    resolved_reqs = []
+    for req in reqs:
+        if req.startswith('-r'):
+            depend_key = req[2:].lstrip()
+            reduce_requirements(depend_key)
+            resolved_reqs += requirements[depend_key]
+        else:
+            resolved_reqs.append(req)
+    requirements[key] = resolved_reqs
+
+
+for k in requirements.keys():
+    reduce_requirements(k)
+
+
+extras_require = {k: v for k, v in requirements.items() if k != 'install'}
+
+
+setup_requires = []
+install_requires = requirements['install']
+tests_require = requirements['test']
+
+
+def find_any_distribution(pkgs):
+    for pkg in pkgs:
+        try:
+            return pkg_resources.get_distribution(pkg)
+        except pkg_resources.DistributionNotFound:
+            pass
+    return None
+
+
+mn_pkg = find_any_distribution(['chainermn'])
+if mn_pkg is not None:
+    msg = """
+We detected that ChainerMN is installed in your environment.
+ChainerMN has been integrated to Chainer and no separate installation
+is necessary. Please uninstall the old ChainerMN in advance.
+"""
+    print(msg)
+    exit(1)
+
+# Currently cupy provides source package (cupy) and binary wheel packages
+# (cupy-cudaXX). Chainer can use any one of these packages.
+cupy_pkg = find_any_distribution([
+    'cupy-cuda92',
+    'cupy-cuda91',
+    'cupy-cuda90',
+    'cupy-cuda80',
+    'cupy',
+])
+if cupy_pkg is not None:
+    req = cupy_requirement(cupy_pkg.project_name)
+    install_requires.append(req)
+    print('Use %s' % req)
 else:
-    long_description = ''
+    print('No CuPy installation detected')
+
+here = os.path.abspath(os.path.dirname(__file__))
+# Get __version__ variable
+exec(open(os.path.join(here, 'chainer', '_version.py')).read())
 
 
-def ensure_scripts(linux_scripts):
-    """Creates the proper script names required for each platform
-    (taken from 4Suite)
-    """
-    from distutils import util
-    if util.get_platform()[:3] == 'win':
-        return linux_scripts + [script + '.bat' for script in linux_scripts]
-    return linux_scripts
-
-
-def get_packages(directory, prefix):
-    """return a list of subpackages for the given directory"""
-    result = []
-    for package in os.listdir(directory):
-        absfile = join(directory, package)
-        if isdir(absfile):
-            if exists(join(absfile, '__init__.py')):
-                if prefix:
-                    result.append('%s.%s' % (prefix, package))
-                else:
-                    result.append(package)
-                result += get_packages(absfile, result[-1])
-    return result
-
-
-def _filter_tests(files):
-    testdir = join('pylint', 'test')
-    return [f for f in files if testdir not in f]
-
-
-class MyInstallLib(install_lib.install_lib):
-    """extend install_lib command to handle package __init__.py and
-    include_dirs variable if necessary
-    """
-    def run(self):
-        """overridden from install_lib class"""
-        install_lib.install_lib.run(self)
-        # manually install included directories if any
-        if include_dirs:
-            for directory in include_dirs:
-                dest = join(self.install_dir, directory)
-                if sys.version_info >= (3, 0):
-                    exclude = set(['invalid_encoded_data*',
-                                   'unknown_encoding*'])
-                else:
-                    exclude = set()
-                shutil.rmtree(dest, ignore_errors=True)
-                shutil.copytree(directory, dest,
-                                ignore=shutil.ignore_patterns(*exclude))
-
-    # override this since pip/easy_install attempt to byte compile test data
-    # files, some of them being syntactically wrong by design, and this scares
-    # the end-user
-    def byte_compile(self, files):
-        files = _filter_tests(files)
-        install_lib.install_lib.byte_compile(self, files)
-
-
-if easy_install_lib:
-    class easy_install(easy_install_lib.easy_install):
-        # override this since pip/easy_install attempt to byte compile
-        # test data files, some of them being syntactically wrong by design,
-        # and this scares the end-user
-        def byte_compile(self, files):
-            files = _filter_tests(files)
-            easy_install_lib.easy_install.byte_compile(self, files)
-
-
-def install(**kwargs):
-    """setup entry point"""
-    if USE_SETUPTOOLS:
-        if '--force-manifest' in sys.argv:
-            sys.argv.remove('--force-manifest')
-    # install-layout option was introduced in 2.5.3-1~exp1
-    elif sys.version_info < (2, 5, 4) and '--install-layout=deb' in sys.argv:
-        sys.argv.remove('--install-layout=deb')
-    packages = [modname] + get_packages(join(base_dir, 'pylint'), modname)
-    if USE_SETUPTOOLS:
-        if install_requires:
-            kwargs['install_requires'] = install_requires
-            kwargs['dependency_links'] = dependency_links
-        kwargs['entry_points'] = {'console_scripts': [
-            'pylint = pylint:run_pylint',
-            'pylint-gui = pylint:run_pylint_gui',
-            'epylint = pylint:run_epylint',
-            'pyreverse = pylint:run_pyreverse',
-            'symilar = pylint:run_symilar',
-        ]}
-    kwargs['packages'] = packages
-    cmdclass = {'install_lib': MyInstallLib,
-                'build_py': build_py}
-    if easy_install_lib:
-        cmdclass['easy_install'] = easy_install
-    return setup(name=distname,
-                 version=__pkginfo__['version'],
-                 license=__pkginfo__['license'],
-                 description=__pkginfo__['description'],
-                 long_description=long_description,
-                 author=__pkginfo__['author'],
-                 author_email=__pkginfo__['author_email'],
-                 url=__pkginfo__['web'],
-                 scripts=ensure_scripts(scripts),
-                 classifiers=__pkginfo__['classifiers'],
-                 data_files=data_files,
-                 ext_modules=ext_modules,
-                 cmdclass=cmdclass,
-                 **kwargs)
-
-if __name__ == '__main__':
-    install()
+setup(
+    name='chainer',
+    version=__version__,  # NOQA
+    description='A flexible framework of neural networks',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Seiya Tokui',
+    author_email='tokui@preferred.jp',
+    url='https://chainer.org/',
+    license='MIT License',
+    packages=['chainer',
+              'chainer.backends',
+              'chainer.dataset',
+              'chainer.datasets',
+              'chainer.distributions',
+              'chainer.exporters',
+              'chainer.functions',
+              'chainer.functions.activation',
+              'chainer.functions.array',
+              'chainer.functions.connection',
+              'chainer.functions.evaluation',
+              'chainer.functions.loss',
+              'chainer.functions.math',
+              'chainer.functions.noise',
+              'chainer.functions.normalization',
+              'chainer.functions.pooling',
+              'chainer.functions.theano',
+              'chainer.functions.util',
+              'chainer.function_hooks',
+              'chainer.iterators',
+              'chainer.initializers',
+              'chainer.links',
+              'chainer.links.activation',
+              'chainer.links.caffe',
+              'chainer.links.caffe.protobuf3',
+              'chainer.links.connection',
+              'chainer.links.loss',
+              'chainer.links.model',
+              'chainer.links.model.vision',
+              'chainer.links.normalization',
+              'chainer.links.theano',
+              'chainer.graph_optimizations',
+              'chainer.optimizers',
+              'chainer.optimizer_hooks',
+              'chainer.serializers',
+              'chainer.testing',
+              'chainer.training',
+              'chainer.training.extensions',
+              'chainer.training.triggers',
+              'chainer.training.updaters',
+              'chainer.utils',
+              'chainermn',
+              'chainermn.communicators',
+              'chainermn.datasets',
+              'chainermn.extensions',
+              'chainermn.functions',
+              'chainermn.iterators',
+              'chainermn.links'],
+    zip_safe=False,
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+)
